618f3ce7f39d9bcc71118ddce9c4aab49fde85bc
Spencer <spengy@gmail.com>
2012-12-10 15:22:28 -0700

    Cleaned up warnings in Classifier
    Unused variables due to the fact that this is an "interface" class. I
    maybe should have made it pure-virtual, but oh well.

--- Core/Classifier.cpp
+++ Core/Classifier.cpp
@@ -10,5 +10,10 @@ Classifier::~Classifier()
 
 bool Classifier::classify(const IntegralImage &image, const QPoint &origin, qreal scale)
 {
+    Q_UNUSED(image)
+    Q_UNUSED(origin)
+    Q_UNUSED(scale)
+
+    //I could have made this class pure-virtual but instead we'll just return false every time
     return false;
 }
